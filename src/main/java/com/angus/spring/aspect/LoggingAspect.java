cdcd12f4d6d76a24d2ae0d9be222514fe21a5631
Angus <lh0528@gmail.com>
2025-03-23 13:14:03 +0800

    新增aop log

---
+++ src/main/java/com/angus/spring/aspect/LoggingAspect.java
@@ -0,0 +1,37 @@
+package com.angus.spring.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.example.foodhistory.controller..*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        logger.info("Entering method: {} with arguments: {}", joinPoint.getSignature().toShortString(), joinPoint.getArgs());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.foodhistory.controller..*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        logger.info("Exiting method: {} with result: {}", joinPoint.getSignature().toShortString(), result);
+    }
+
+    @Around("execution(* com.example.foodhistory.controller..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        logger.info("duration {} ms", executionTime);
+        return proceed;
+    }
+}
--- src/main/java/com/example/foodhistory/FoodHistoryApplication.java
+++ src/main/java/com/example/foodhistory/FoodHistoryApplication.java
@@ -2,8 +2,12 @@ package com.example.foodhistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.angus.spring.aspect.LoggingAspect;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {LoggingAspect.class, FoodHistoryApplication.class})
 public class FoodHistoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(FoodHistoryApplication.class, args);
